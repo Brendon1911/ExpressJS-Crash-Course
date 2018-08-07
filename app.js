952eca334b50766c810d76750f0796d7d74d640f
Unknown <brendonrichards93@gmail.com>
2018-08-07 11:07:33 +0200

    Add views

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const app = express();
 
 // app.use(logger);
 
+// View Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("index", { title: "Customers" });
 });
 
 app.listen(3000, () => {
