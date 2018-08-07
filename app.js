e0e9039c54ae7929a5242d77016e868a55c92dd7
Unknown <brendonrichards93@gmail.com>
2018-08-07 11:20:11 +0200

    Add users

--- app.js
+++ app.js
@@ -22,8 +22,32 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname, "public")));
 
+let users = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "johndoe@gmail.com"
+  },
+  {
+    id: 2,
+    firstName: "Bob",
+    lastName: "Smith",
+    email: "bobsmith@gmail.com"
+  },
+  {
+    id: 3,
+    firstName: "Jill",
+    lastName: "Jackson",
+    email: "jilljackson@gmail.com"
+  }
+]
+
 app.get("/", (req, res) => {
-  res.render("index", { title: "Customers" });
+  res.render("index", { 
+    title: "Customers",
+    users: users
+  });
 });
 
 app.listen(3000, () => {
